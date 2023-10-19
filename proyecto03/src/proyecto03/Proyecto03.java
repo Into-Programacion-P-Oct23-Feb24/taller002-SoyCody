946c83c684c610b4e5db816709afe299a1335806
Cody <codycabreraa@gmail.com>
2023-10-19 16:46:18 -0500

    tercer commit

---
+++ proyecto03/src/proyecto03/Proyecto03.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto03;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Proyecto03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo de Java");
+        System.out.println("Manejado desde GitHub");
+        System.out.println("Versionado a traves de Git");
+        System.out.println("Creado por SoyCody");
+    }
+    
+}
